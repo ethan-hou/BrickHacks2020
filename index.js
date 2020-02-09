68bfd6cefdeabccaed016a8e7d021223a7ec17d2
Benjamin Nguyen <benjamin@.github.com>
2020-02-09 00:44:02 -0500

    Update index.js
    update cors

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var socket = require('socket.io');
 var cors = require('cors');
-app.use(cors({origin: "*:*"}))
+app.use(cors({origin: "medievalrule.herokuapp.com"}))
 // app.get('/', function(req, res){
 //     res.send('<h1>Hello world</h1>');    
 //   });
