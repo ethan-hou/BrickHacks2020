627ee66578cae25091ec32942c5f4773b0378095
Benjamin Nguyen <benjamin@.github.com>
2020-02-08 23:22:56 -0500

    made changes

--- Chat/index.js
+++ Chat/index.js
@@ -1,27 +1,39 @@
 var express = require('express');
+var app = require('express')();
+var http = require('http').createServer(app);
 var socket = require('socket.io');
-// App setup
-var app = express();
+var cors = require('cors');
+// app.use(cors({origin: false}))
+// app.get('/', function(req, res){
+//     res.send('<h1>Hello world</h1>');    
+//   });
 
-var server = app.listen(2020, function(){
-  console.log('listening to requests on port 2020,')
-});
+// App setup
+http.listen(2020, function(){
+    console.log('Server is listening to requests on port 2020')
+  });
 
 // Static files
 app.use(express.static('public'));
 
 // Socket setup
-var io = socket(server);
+var io = socket(http);
+players = {};
 
 io.on('connection', function(socket) {
   console.log('made socket connection', socket.id);
-
+    players[socket.id] = {
+        name: "test",
+        role: "reara"
+    };
+    console.log(players)
 //Handle chat event
   socket.on('chat', function(data){
-    io.sockets.emit('chat', data);
+     io.sockets.emit('chat', data);
+    // io.broadcast.emit('chat', data);
   });
 
   socket.on('typing', function(data){
-    socket.broadcast.emit('typing', data)
+    socket.broadcast.emit('typing', data);
   });
 });
