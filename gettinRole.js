9d8c441748aa17e5a347d729424d6dded667b934
ethan-hou <ethanykhou@gmail.com>
2020-02-09 01:07:15 -0500

    Timer and role display

--- gettinRole.js
+++ gettinRole.js
@@ -1,14 +1,11 @@
 //takes player list and assigns a random role from roleList to playerList
-function roleSelect(playerList,roleList)
-{
+function roleSelect(playerList, roleList) {
     roleList = roleList.toArray();
     var hold = 0;
-    while (roleList.length !=  0)
-    {
-        hold = Math.floor((Math.random()*15));
+    while (roleList.length != 0) {
+        hold = Math.floor((Math.random() * 15));
         var index = roleList.indexOf(hold);
-        if (index>-1 && hold<= roleList.length)
-        {
+        if (index > -1 && hold <= roleList.length) {
             playerList.add(roleList.get(hold));
             roleList.splice(index, hold);
         }
--- roleSelect.js
+++
@@ -1,5 +0,0 @@
-function myFunction() {
-    setTimeout(function() {
-        window.location = 'inGame.html';
-    }, 5);
-}
